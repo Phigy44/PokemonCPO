ff1f451f0c1e85fc3edb0e8545ac77ee0ee3ba1c
Phigy44 <31017193+Phigy44@users.noreply.github.com>
2025-09-14 17:17:51 +0200

    Added Opening of cards

--- MainGame.gd
+++ MainGame.gd
@@ -4,6 +4,8 @@ extends Node2D
 
 var card_data_list = []
 var rng = RandomNumberGenerator.new()
+
+var packSize = 5
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Testing
@@ -16,22 +18,34 @@ func _ready():
 		{"name": "Glurak", "rarity": "Legendary", "image": "res://assets/cards/005_Glutexo.png"},
 	]
 	rng.randomize()
-	spawn_cards()
+	
 
 func open_Pack_pressed():
 	print("New Pack")
+	#Alte Karten entfernen
+	for child in get_children():
+		if child is Node2D:
+			if child != $Camera2D: # Kamera nicht löschen.
+				child.queue_free()
 
+	# Karten zufällig anzeigen
+	var pack = []
+	for i in range(packSize):
+		var random_index = rng.randi_range(0, card_data_list.size() - 1)
+		pack.append(card_data_list[random_index])
+		
 
-func spawn_cards():
+	#Karten nebeneinander Darstellen
+
 	var spacing = 260 # abstand zwischen den Karten
-	for i in range(card_data_list.size()):
+	for i in range(pack.size()):
 		var card = card_scene.instantiate()
 		add_child(card)
 		#Position berechnen
 		
 		
-		var x = (i - (card_data_list.size() - 1 / 2.0)) * spacing
+		var x = (i - (pack.size() - 1 / 2.0)) * spacing
 		card.position = Vector2(x, 0)
 		#Daten setzten
 		print(x)
-		card.set_card(card_data_list[i])
+		card.set_card(pack[i])
