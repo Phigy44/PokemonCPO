7186ef2576ce0fc1656d1aa3d3c9aff2a8c05368
Phigy44 <31017193+Phigy44@users.noreply.github.com>
2025-09-14 16:30:30 +0200

    Added new Card' Changes to MainGame (Camera and More Test Data)
    Added Card as Scen to instantiate

--- MainGame.gd
+++ MainGame.gd
@@ -1,21 +1,31 @@
 extends Node2D
 
-@onready var card = $Card #Referenz für die Card
+@export var card_scene : PackedScene
 
+var card_data_list = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var bisasam_data = {
-		"name": "Bisasam",
-		"rarity" : "Rare",
-		"image" : "res://assets/cards/001_Bisasam.png"
+	#Testing
+	card_data_list = [
+		{"name":"Bisasam", "rarity":"common", "image":"res://assets/cards/001_Bisasam.png"},
+		{"name":"Bisaknops", "rarity":"rare", "image":"res://assets/cards/002_Bisaknosp.png"},
+		{"name":"Bisaflor", "rarity":"Legendary", "image":"res://assets/cards/003_Bisaflor.png"},
+	]
 
-	}
+	spawn_cards()
 
-	var bisaknops_data = {
-		"name": "Bisaknosp",
-		"rarity" : "Common",
-		"image" : "res://assets/cards/002_Bisaknosp.png"
 
-	}
 
-	card.set_card(bisasam_data)
+func spawn_cards():
+	var spacing = 200 # abstand zwischen den Karten
+	for i in range(card_data_list.size()):
+		var card = card_scene.instantiate()
+		add_child(card)
+		#Position berechnen
+		
+		
+		var x = (i -(card_data_list.size() - 1 / 2.0)) * spacing
+		card.position = Vector2(x,0)
+		#Daten setzten
+	
+		card.set_card(card_data_list[i])
